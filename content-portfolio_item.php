19f693fbfe5877fc10bcf14cb435df2ec42e0083
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-07-28 23:33:03 +0300

    Let me think will I add support for portfolio. I might.

--- content-portfolio_item.php
+++ content-portfolio_item.php
@@ -6,20 +6,43 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title" ' . hybrid_get_attr( 'entry-title' ) . '>', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php hybrid_attr( 'post' ); ?>>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'kuorinka' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+	<?php if ( is_singular( get_post_type() ) ) : // If viewing a single post. ?>
+	
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title" ' . hybrid_get_attr( 'entry-title' ) . '>', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'kuorinka' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+		
+		<footer class="entry-footer">
+			<?php kuorinka_post_terms( array( 'taxonomy' => 'portfolio' ) ); ?>
+		</footer><!-- .entry-footer -->
+		
+	<?php else : // If not viewing a single post. ?>
+	
+		<header class="entry-header">
+	
+			<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'kuorinka-large', array( 'class' => 'thumbnail-large' ) ); ?>
+		
+			<?php the_title( sprintf( '<h1 class="entry-title" ' . hybrid_get_attr( 'entry-title' ) . '><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	
+		</header><!-- .entry-header -->
+		
+		<div class="entry-summary" <?php hybrid_attr( 'entry-summary' ); ?>>
+			<?php the_excerpt(); ?>
+			<?php wp_link_pages(); ?>
+		</div><!-- .entry-summary -->
+
+	<?php endif; // End single post check. ?>
 
 </article><!-- #post-## -->
