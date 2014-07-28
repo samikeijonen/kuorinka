feaf29b6f1eefb7466857a77096a1dbebaaef848
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-07-28 23:36:10 +0300

    Add content attribute for Schema.

--- pages/front-page.php
+++ pages/front-page.php
@@ -10,7 +10,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main" <?php hybrid_attr( 'content' ); ?>>
 		
 		<?php get_sidebar( 'front-page' ); // Loads the sidebar-front-page.php template. ?>
 		
