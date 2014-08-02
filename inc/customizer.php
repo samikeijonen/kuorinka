64c6300f1a224913df436b5c1e4b1a5583a648b6
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-08-02 22:21:23 +0300

    Remove logo upload.

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,43 +11,11 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function kuorinka_customize_register( $wp_customize ) {
-
-	/* === Logo upload. === */
-	
-	/* Add the theme section. */
-	$wp_customize->add_section(
-		'logo',
-		array(
-			'title'      => esc_html__( 'Logo', 'kuorinka' ),
-			'priority'   => 10
-		)
-	);
-	
-	/* Add the 'logo' setting. */
-	$wp_customize->add_setting(
-		'logo_upload',
-		array(
-			'default'           => '',
-			'sanitize_callback' => 'esc_url_raw'
-		)
-	);
-	
-	/* Add custom logo control. */
-	$wp_customize->add_control(
-		new WP_Customize_Image_Control(
-			$wp_customize, 'logo_image',
-				array(
-					'label'    => esc_html__( 'Upload custom logo.', 'kuorinka' ),
-					'section'  => 'logo',
-					'settings' => 'logo_upload',
-					'priority' => 10,
-			) 
-		) 
-	);
 	
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	
 }
 add_action( 'customize_register', 'kuorinka_customize_register' );
 
