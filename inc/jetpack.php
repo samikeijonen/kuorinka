016fe4bfcefd82c9f86e3a9dff838fad07eb5c37
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-08-02 22:22:00 +0300

    Add footer_widgets to infinite scroll.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -11,9 +11,13 @@
  * See: http://jetpack.me/support/infinite-scroll/
  */
 function kuorinka_jetpack_setup() {
+
+	/* Add support for infinite scroll. */
 	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'footer'    => 'page',
+		'container'      => 'main',
+		'footer'         => 'page',
+		'footer_widgets' => 'subsidiary'
 	) );
+	
 }
 add_action( 'after_setup_theme', 'kuorinka_jetpack_setup' );
