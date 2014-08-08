cd8568cc2ceef79876a5e33f7507c5f982209192
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-08-08 11:13:27 +0300

    Use entry-meta.php template.

--- content.php
+++ content.php
@@ -12,14 +12,7 @@
 			<?php the_title( '<h1 class="entry-title" ' . hybrid_get_attr( 'entry-title' ) . '>', '</h1>' ); ?>
 		</header><!-- .entry-header -->
 	
-		<?php if ( 'post' == get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php kuorinka_posted_on(); ?>
-				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-					<span class="comments-link"><?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?></span>
-				<?php endif; ?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php get_template_part( 'entry', 'meta' ); // Loads the entry-meta.php template. ?>
 		
 		<div class="entry-content" <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php the_content(); ?>
@@ -46,14 +39,7 @@
 		
 		</header><!-- .entry-header -->
 		
-		<?php if ( 'post' == get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php kuorinka_posted_on(); ?>
-				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-					<span class="comments-link"><?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?></span>
-				<?php endif; ?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php get_template_part( 'entry', 'meta' ); // Loads the entry-meta.php template. ?>
 		
 		<div class="entry-summary" <?php hybrid_attr( 'entry-summary' ); ?>>
 			<?php the_excerpt(); ?>
