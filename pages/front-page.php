fe1284f15b7f0762b98dc10996a4d11ef5268c5c
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-08-24 20:26:28 +0300

    Add check do we want to show posts on front page or not.

--- pages/front-page.php
+++ pages/front-page.php
@@ -17,106 +17,114 @@ get_header(); ?>
 		<?php get_sidebar( 'front-page' ); // Loads the sidebar-front-page.php template. ?>
 		
 		<?php
-		/* Get the sticky posts. */
-		$sticky = get_option( 'sticky_posts' );
 		
-		$kuorinka_sticky_args = apply_filters( 'kuorinka_front_page_sticky_arguments', array(
-			'post_type'   => array( 'post' ),
-			'post__in'    => $sticky,
-			'post_status' => 'publish'
-		) );
+		/* Whether to show posts or not. Defaults to true. */
+		$kuorinka_show_front_page_posts = apply_filters( 'kuorinka_show_front_page_posts', true );
 		
-		if ( ! empty( $sticky ) ) :
+		if( false === $kuorinka_show_front_page_posts || !get_theme_mod( 'hide_front_page_posts') ) : // Check do we show posts.
 		
-			/* Set transient (24h) for faster loading. Delete transient on hook 'save_post' in functions.php file. */
-			if( false === ( $kuorinka_sticky_query = get_transient( 'kuorinka_sticky_query' ) ) ) {
-				$kuorinka_sticky_query = new WP_Query( $kuorinka_sticky_args );
-				set_transient( 'kuorinka_sticky_query', $kuorinka_sticky_query, 60*60*24 );
-			}
+			/* Get the sticky posts. */
+			$sticky = get_option( 'sticky_posts' );
+		
+			$kuorinka_sticky_args = apply_filters( 'kuorinka_front_page_sticky_arguments', array(
+				'post_type'   => array( 'post' ),
+				'post__in'    => $sticky,
+				'post_status' => 'publish'
+			) );
+		
+			if ( ! empty( $sticky ) ) :
+		
+				/* Set transient (24h) for faster loading. Delete transient on hook 'save_post' in functions.php file. */
+				if( false === ( $kuorinka_sticky_query = get_transient( 'kuorinka_sticky_query' ) ) ) {
+					$kuorinka_sticky_query = new WP_Query( $kuorinka_sticky_args );
+					set_transient( 'kuorinka_sticky_query', $kuorinka_sticky_query, 60*60*24 );
+				}
 			
-			if ( $kuorinka_sticky_query->have_posts() ) :
+				if ( $kuorinka_sticky_query->have_posts() ) :
 
-				while ( $kuorinka_sticky_query->have_posts() ) : $kuorinka_sticky_query->the_post(); ?>
+					while ( $kuorinka_sticky_query->have_posts() ) : $kuorinka_sticky_query->the_post(); ?>
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-						<header class="entry-header">
-							<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-						</header><!-- .entry-header -->
+							<header class="entry-header">
+								<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+							</header><!-- .entry-header -->
 
-						<div class="entry-summary">
-							<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'thumbnail', array( 'class' => 'thumbnail' ) ); ?>
-							<?php the_excerpt(); ?>
-						</div><!-- .entry-summary -->
+							<div class="entry-summary">
+								<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'thumbnail', array( 'class' => 'thumbnail' ) ); ?>
+								<?php the_excerpt(); ?>
+							</div><!-- .entry-summary -->
 
-					</article><!-- #post-## -->
+						</article><!-- #post-## -->
 
-				<?php endwhile; // End while loop. ?>
+					<?php endwhile; // End while loop. ?>
 
-			<?php endif; // End check for posts. ?>
+				<?php endif; // End check for posts. ?>
 			
-		<?php endif; // End check for sticky posts. ?>
+			<?php endif; // End check for sticky posts. ?>
 		
-		<?php wp_reset_postdata(); // reset query. ?>
-		
-		<?php
-		/* Get other than sticky posts. */
-		
-		$kuorinka_post_args = apply_filters( 'kuorinka_front_page_post_arguments', array(
-			'post_type'      => array( 'post' ),
-			'post__not_in'   => $sticky,
-			'post_status'    => 'publish',
-			'posts_per_page' => 4,
-			'tax_query'      => array(
-				array(
-					'taxonomy' => 'post_format',
-					'field'    => 'slug',
-					'terms'    => array( 
-						'post-format-aside',
-						'post-format-audio',
-						'post-format-chat',
-						'post-format-link',
-						'post-format-quote',
-						'post-format-status',
-					),
-					'operator' => 'NOT IN'
-				)
-			)
-		) ); ?>
-		
-		<section id="kuorinka-front-page-posts" class="kuorinka-front-page-section kuorinka-front-page-posts">
+			<?php wp_reset_postdata(); // reset query. ?>
 		
 			<?php
-			/* Set transient (24h) for faster loading. Delete transient on hook 'save_post' in functions.php file. */
-			if( false === ( $kuorinka_post_query = get_transient( 'kuorinka_post_query' ) ) ) {
-				$kuorinka_post_query = new WP_Query( $kuorinka_post_args );
-				set_transient( 'kuorinka_post_query', $kuorinka_post_query, 60*60*24 );
-			}
+			/* Get other than sticky posts. */
+		
+			$kuorinka_post_args = apply_filters( 'kuorinka_front_page_post_arguments', array(
+				'post_type'      => array( 'post' ),
+				'post__not_in'   => $sticky,
+				'post_status'    => 'publish',
+				'posts_per_page' => 4,
+				'tax_query'      => array(
+					array(
+						'taxonomy' => 'post_format',
+						'field'    => 'slug',
+						'terms'    => array( 
+							'post-format-aside',
+							'post-format-audio',
+							'post-format-chat',
+							'post-format-link',
+							'post-format-quote',
+							'post-format-status',
+						),
+						'operator' => 'NOT IN'
+					)
+				)
+			) ); ?>
+		
+			<section id="kuorinka-front-page-posts" class="kuorinka-front-page-section kuorinka-front-page-posts">
+		
+				<?php
+				/* Set transient (24h) for faster loading. Delete transient on hook 'save_post' in functions.php file. */
+				if( false === ( $kuorinka_post_query = get_transient( 'kuorinka_post_query' ) ) ) {
+					$kuorinka_post_query = new WP_Query( $kuorinka_post_args );
+					set_transient( 'kuorinka_post_query', $kuorinka_post_query, 60*60*24 );
+				}
 			
-			if ( $kuorinka_post_query->have_posts() ) :
+				if ( $kuorinka_post_query->have_posts() ) :
 
-				while ( $kuorinka_post_query->have_posts() ) : $kuorinka_post_query->the_post(); ?>
+					while ( $kuorinka_post_query->have_posts() ) : $kuorinka_post_query->the_post(); ?>
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-						<header class="entry-header">
-							<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'kuorinka-large', array( 'class' => 'thumbnail-large' ) ); ?>
-							<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-						</header><!-- .entry-header -->
+							<header class="entry-header">
+								<?php if ( has_post_thumbnail() ) the_post_thumbnail( 'kuorinka-large', array( 'class' => 'thumbnail-large' ) ); ?>
+								<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+							</header><!-- .entry-header -->
 
-						<div class="entry-summary">
-							<?php the_excerpt(); ?>
-						</div><!-- .entry-summary -->
+							<div class="entry-summary">
+								<?php the_excerpt(); ?>
+							</div><!-- .entry-summary -->
 
-					</article><!-- #post-## -->
+						</article><!-- #post-## -->
 
-				<?php endwhile; // End while loop. ?>
+					<?php endwhile; // End while loop. ?>
 
-			<?php endif; // End check for posts. ?>
+				<?php endif; // End check for posts. ?>
 			
-			<?php wp_reset_postdata(); // reset query. ?>
+				<?php wp_reset_postdata(); // reset query. ?>
 			
-		</section><!-- #kuorinka-front-page-posts -->
+			</section><!-- #kuorinka-front-page-posts -->
+			
+		<?php endif; // End check for show posts. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
