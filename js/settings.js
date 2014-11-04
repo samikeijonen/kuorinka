b8e0d117f7e959560f86d99a852970c62652c3d8
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-11-04 18:51:25 +0200

    Use .main-navigation class so that navigation works with JS disabled.

--- js/settings.js
+++ js/settings.js
@@ -6,7 +6,7 @@
         customToggle: "#nav-toggle", // Selector: Specify the ID of a custom toggle
         closeOnNavClick: false, // Boolean: Close the navigation when one of the links are clicked
         openPos: "relative", // String: Position of the opened nav, relative or static
-        navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
+        //navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
         navActiveClass: "js-nav-active", // String: Class that is added to <html> element when nav is active
         jsClass: "js", // String: 'JS enabled' class which is added to <html> element
         init: function(){}, // Function: Init callback
