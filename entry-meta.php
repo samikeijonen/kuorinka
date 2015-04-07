aa663180c31944468d865f6baf75918d3868150b
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-04-07 18:38:23 +0300

    Use default comments_pop_up arguments because they will have lot's of accessibility love in WP 4.2.

--- entry-meta.php
+++ entry-meta.php
@@ -2,7 +2,7 @@
 	<div class="entry-meta">
 		<?php kuorinka_posted_on(); ?>
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<span class="comments-link"><?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?></span>
+			<span class="comments-link"><?php comments_popup_link( false, false, false, 'comments-link', false ); ?></span>
 		<?php endif; ?>
 	</div><!-- .entry-meta -->
 <?php endif;
