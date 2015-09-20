3b2aba4fe2d0534bded13d5ecc25eb24794ed93c
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-09-20 12:45:26 +0300

    Remove admin header callbacks and styles. They are all in the Customizer now.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -20,8 +20,6 @@ function kuorinka_custom_header_setup() {
 		'height'                 => 500,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'kuorinka_header_style',
-		'admin-head-callback'    => 'kuorinka_admin_header_style',
-		'admin-preview-callback' => 'kuorinka_admin_header_image',
 	) ) );
 
 }
@@ -54,53 +52,3 @@ function kuorinka_header_style() {
 	echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
 }
 endif; // kuorinka_header_style
-
-if ( ! function_exists( 'kuorinka_admin_header_style' ) ) :
-/**
- * Callback function for outputting the custom header CSS to `admin_head` on "Appearance > Custom Header".  See 
- * the `css/admin-custom-header.css` file for all the style rules specific to this screen.
- *
- * @see kuorinka_custom_header_setup().
- */
-function kuorinka_admin_header_style() {
-
-	/* Get out if we don't use header text. */
-	if ( !display_header_text() ) {
-		return;
-	}
-
-	$header_color = get_header_textcolor();
-	
-	/* Get out if we don't have header text text color. */
-	if ( empty( $header_color ) ) {
-		return;
-	}
-
-	$style = '';
-
-	$style .= "#site-title, #site-title a { color: #{$header_color} }";
-
-	echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
-
-}
-endif; // kuorinka_admin_header_style
-
-if ( ! function_exists( 'kuorinka_admin_header_image' ) ) :
-/**
- * Custom header image markup displayed on the Appearance > Header admin panel.
- *
- * @see kuorinka_custom_header_setup().
- */
-function kuorinka_admin_header_image() {
-	$style = sprintf( ' style="color:#%s;"', get_header_textcolor() );
-?>
-	<div id="headimg">
-		<h1 id="site-title" class="displaying-header-text"><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div id="site-description" class="displaying-header-text"><?php bloginfo( 'description' ); ?></div>
-		<?php if ( get_header_image() ) : ?>
-			<img src="<?php header_image(); ?>" alt="">
-		<?php endif; ?>
-	</div>
-<?php
-}
-endif; // kuorinka_admin_header_image
